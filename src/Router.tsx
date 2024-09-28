cb8b153726383cdf85bdf1a04b7f8473d50a419e
emrecansonmez <131684329+emrecansonmez@users.noreply.github.com>
2024-09-28 12:39:33 +0300

    todo-app structure
    app structure is created
    routes are created
    antd is added

---
+++ src/Router.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./pages/Auth/Login";
+import { Register } from "./pages/Auth/Register";
+import { DashboardLayout } from "./modules/Dashboard";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/dashboard" element={<DashboardLayout />}></Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
